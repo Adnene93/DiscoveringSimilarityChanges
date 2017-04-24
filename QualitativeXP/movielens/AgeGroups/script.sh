8474876e9d7c5c5fe7f77fe88211289ca2d9802d
BELFODIL Adnene <adnene.belfodil@insa-lyon.fr>
2017-04-24 14:20:09 +0200

    Qualitative Experiments

--- QualitativeXP/movielens/AgeGroups/script.sh
+++
@@ -1,4 +0,0 @@
-python C:\\Users\\Adnene\\workspace\\PaDMiV\\main.py qualitative.json 
-# pypy C:\\Users\\Adnene\\workspace\\PaDMiV\\main.py nb_users.json
-# pypy C:\\Users\\Adnene\\workspace\\PaDMiV\\main.py nb_attr_items.json
-# pypy C:\\Users\\Adnene\\workspace\\PaDMiV\\main.py nb_attr_users.json
